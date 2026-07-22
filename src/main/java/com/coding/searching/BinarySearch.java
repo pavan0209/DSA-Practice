0d9aa50df2d520bdf7ac1d9956a330601dfa386f
pavan0209 <pavanasonawane@gmail.com>
2026-07-22 22:48:21 +0530

    binary search implementation

--- src/main/java/com/coding/searching/BinarySearch.java
+++ src/main/java/com/coding/searching/BinarySearch.java
@@ -8,7 +8,7 @@ public class BinarySearch {
         int start = 0, end = array.length - 1;
 
         while (start <= end) {
-            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
 
             if (array[mid] == key) {
                 index = mid;
@@ -26,7 +26,7 @@ public class BinarySearch {
     public static void main(String[] args) {
 
         int[] array = {1, 2, 5, 89, 90, 95, 100};
-        int key = 10;
+        int key = 5;
 
         if (searchElement(array, key))
             System.out.println("KEY FOUND AT INDEX :: " + index);
